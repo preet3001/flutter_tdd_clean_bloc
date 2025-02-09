472f7379bc704e5fa4163a1c72480c8454656b12
Preet Singh <preetsingh3001@gmail.com>
2025-02-09 16:05:45 +0530

    red: product model from json

--- test/feature/product_listing/data/model/product_model_test.dart
+++ test/feature/product_listing/data/model/product_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_tdd_clean_bloc/features/product_listing/data/models/product_model.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/domain/entities/product_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   const ProductModel tModel = ProductModel(
@@ -11,5 +15,14 @@ void main() {
   );
   test('should be of ProductEntity type', () {
     expect(tModel, isA<ProductEntity>());
+  });
+  group('Product Model fromJson', () {
+    final data = json.decode(fixture('products.json'));
+    const expected = ProductModel(
+      title: 'test',
+      description: 'test',
+      price: 0,
+      thumbnail: '',
+    );
   });
 }
