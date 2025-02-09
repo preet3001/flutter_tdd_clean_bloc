59f71ae2531e379c7257cdafb8cd08b9fb049e6f
Preet Singh <preetsingh3001@gmail.com>
2025-02-09 19:09:22 +0530

    green: product bloc fetchProducts event

--- lib/features/product_listing/presentation/bloc/product_bloc.dart
+++ lib/features/product_listing/presentation/bloc/product_bloc.dart
@@ -1,9 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_tdd_clean_bloc/core/usecase/usecase.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/domain/usecases/get_products.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/presentation/bloc/product_event.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/presentation/bloc/product_state.dart';
 
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
-  ProductBloc(this.getProducts) : super(ProductInitialState()) {}
+  ProductBloc(this.getProducts) : super(ProductInitialState()) {
+    on<FetchProduct>((event, emit) => getProducts(NoParam()));
+  }
   final GetProducts getProducts;
 }
