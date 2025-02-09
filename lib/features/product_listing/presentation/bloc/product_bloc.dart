9093e02946095fe0a89c5b3e77e4e33443d92472
Preet Singh <preetsingh3001@gmail.com>
2025-02-09 19:52:04 +0530

    green: product_bloc event handling

--- lib/features/product_listing/presentation/bloc/product_bloc.dart
+++ lib/features/product_listing/presentation/bloc/product_bloc.dart
@@ -1,12 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_tdd_clean_bloc/core/usecase/usecase.dart';
+import 'package:flutter_tdd_clean_bloc/core/util/result.dart';
+import 'package:flutter_tdd_clean_bloc/features/product_listing/domain/entities/product_entity.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/domain/usecases/get_products.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/presentation/bloc/product_event.dart';
 import 'package:flutter_tdd_clean_bloc/features/product_listing/presentation/bloc/product_state.dart';
 
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
   ProductBloc(this.getProducts) : super(ProductInitialState()) {
-    on<FetchProduct>((event, emit) => getProducts(NoParam()));
+    on<FetchProduct>(_fetchProduct);
   }
   final GetProducts getProducts;
+  late ProductEntity entity;
+
+  Future<void> _fetchProduct(
+    ProductEvent event,
+    Emitter<ProductState> emit,
+  ) async {
+    emit(ProductLoadingState());
+    final result = await getProducts(NoParam());
+    switch (result) {
+      case Ok():
+        emit(ProductCompletedState(result.value));
+      default:
+        emit(const ProductFailureState('Some thing went wrong'));
+    }
+  }
 }
